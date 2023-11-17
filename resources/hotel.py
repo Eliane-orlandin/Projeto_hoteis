af822a58df70e502816cb847c03d8181929d7c0e
Eliane <liorlandin33@gmail.com>
2023-11-17 16:20:26 -0300

    Implementação do método GET para obter hotel por ID

--- resources/hotel.py
+++ resources/hotel.py
@@ -49,3 +49,11 @@ hoteis = [
 class Hoteis (Resource):
     def get (self):
         return {"hoteis" : hoteis}
+    
+class Hotel (Resource):
+    def get (self, hotel_id):
+        for hotel in hoteis:
+            if hotel ['hotel_id'] == hotel_id:
+                return hotel
+        return {'message' : 'Hotel not found.'}, 404
+        
