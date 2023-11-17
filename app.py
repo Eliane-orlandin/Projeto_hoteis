df775af69dfced858b92755bceebf062255220db
Eliane <liorlandin33@gmail.com>
2023-11-17 11:07:50 -0300

    Adição da classe Hotéis, representa o recurso de pesquisa.

--- app.py
+++ app.py
@@ -3,3 +3,9 @@ from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api= Api(app)
+
+class Hoteis (Resource):
+    def get (self):
+        return {"hoteis", "meus hoteis"}
+
+api.add_resource(Hoteis, "/hoteis")
