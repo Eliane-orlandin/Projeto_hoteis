079cea84c96317679ecb3a44fb67605f8c6f91c9
Eliane <liorlandin33@gmail.com>
2023-11-17 17:02:55 -0300

    Implementação do método POST para adicionar um novo hotel

--- resources/hotel.py
+++ resources/hotel.py
@@ -1,4 +1,4 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 
 hoteis = [
         {
@@ -56,4 +56,26 @@ class Hotel (Resource):
             if hotel ['hotel_id'] == hotel_id:
                 return hotel
         return {'message' : 'Hotel not found.'}, 404
+
+    def post (self, hotel_id):
+        argumentos = reqparse.RequestParser()
+        argumentos.add_argument('nome')
+        argumentos.add_argument('estrelas')
+        argumentos.add_argument('diaria')
+        argumentos.add_argument('cidade')
+
+        dados = argumentos.parse_args()
+
+        novo_hotel = {
+            'hotel_id' : hotel_id,
+            'nome' : dados['nome'], 
+            'estrelas' : dados['estrelas'],
+            'diaria' : dados['diaria'],
+            'cidade' : dados['cidade']
+        }
+
+        hoteis.append(novo_hotel)
+        return novo_hotel, 200
+    
+
         
