7a1c7d354e8dc384ad6f0940cf0dd5eeeafc0838
Eliane <liorlandin33@gmail.com>
2023-11-27 09:51:52 -0300

    Adicionada rota e método para obter informações do usuário por ID

---
+++ resources/usuario.py
@@ -0,0 +1,10 @@
+from flask_restful import Resource, reqparse
+from models.usuario import UserModel
+    
+class User (Resource):
+
+    def get (self, user_id):
+        user = UserModel.find_user(user_id)
+        if user:
+            return user.json()
+        return {'message' : 'User not found.'}, 404
