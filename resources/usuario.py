100ed5d7b02156b482819db07297afa7cc68faea
Eliane <liorlandin33@gmail.com>
2023-11-27 09:58:31 -0300

    Implementado método para excluir usuário por ID na rota correspondente

--- resources/usuario.py
+++ resources/usuario.py
@@ -8,3 +8,11 @@ class User (Resource):
         if user:
             return user.json()
         return {'message' : 'User not found.'}, 404
+    
+    def delete(self, user_id):
+        user = UserModel.find_user(user_id)
+        if user:
+            user.delete_user()
+            return {"message" : "User deleted."}
+        return {'message' : 'User not found.'}, 404 
+    
