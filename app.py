40429ddaaa4b0626b1b0632c31becdb938157688
Eliane <liorlandin33@gmail.com>
2023-11-27 17:20:59 -0300

    Incluídos imports necessários para autenticação com JWT no app.py e adicionada rota para endpoint de login.

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from flask_restful import Api
 from resources.hotel import Hoteis, Hotel
-from resources.usuario import User 
+from resources.usuario import User , UserRegister, UserLogin
+from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
 app.config ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///banco.db'
 app.config ['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 api= Api(app)
+jwt = JWTManager(app)
 
 @app.before_request
 def cria_banco():
@@ -15,6 +17,8 @@ def cria_banco():
 api.add_resource(Hoteis, '/hoteis')
 api.add_resource(Hotel, '/hoteis/<string:hotel_id>')
 api.add_resource(User, "/usuarios/<int:user_id>")
+api.add_resource(UserRegister, '/cadastro')
+api.add_resource(UserLogin, "/login")
 
 if __name__ == '__main__':
     from sql_alchemy import banco
